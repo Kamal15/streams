92243562c0fb79d2175de1ce312a2415859658e4
Kamal <kamal@nmsworks.co.in>
2017-09-07 17:18:25 +0530

    Secure Kafka Stream Processing Example added.

---
+++ src/main/java/io/confluent/examples/streams/SecureKafkaStreamsExample.java
@@ -0,0 +1,39 @@
+package io.confluent.examples.streams;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+import java.util.Properties;
+
+/**
+ * The purpose of this example is to configure Kafka Streams for secure stream processing.
+ */
+public class SecureKafkaStreamsExample {
+
+    public static void main(String[] args) {
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "secure-kafka-streams-app");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "secure-kafka-streams-app-client");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass());
+
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/etc/security/tls/kafka.client.truststore.jks");
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "test1234");
+        props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/etc/security/tls/kafka.client.keystore.jks");
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "test1234");
+        props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "test1234");
+
+        final KStreamBuilder builder = new KStreamBuilder();
+        builder.stream("secure-input").to("secure-output");
+        final KafkaStreams streams = new KafkaStreams(builder, props);
+        streams.cleanUp();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        streams.start();
+    }
+}
